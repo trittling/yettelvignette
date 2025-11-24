7466c7e3b584bc3455096f64ffeea5da51ca5190
Tamás Rittling <tamas.rittling@hiflylabs.com>
2025-11-24 09:25:24 +0100

    Initial Commit

---
+++ YettelVignette/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YettelVignette
+//
+//  Created by Rittling Tamás on 2025. 11. 24..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YettelVignette/YettelVignetteApp.swift
@@ -0,0 +1,17 @@
+//
+//  YettelVignetteApp.swift
+//  YettelVignette
+//
+//  Created by Rittling Tamás on 2025. 11. 24..
+//
+
+import SwiftUI
+
+@main
+struct YettelVignetteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
